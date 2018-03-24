e5cd0f30888ce9df07507a88260e9caae03b88b7
Tudor Popescu <tudor@adonissoft.com>
2018-03-24 14:48:24 +0200

    revert default notebook

--- src/components/App.vue
+++ src/components/App.vue
@@ -41,7 +41,7 @@
         data() {
             return {
                 title: 'Cluedo Notebook',
-                displayNotebook: true
+                displayNotebook: false
             }
         },
         beforeMount() {
